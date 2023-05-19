c6ceed778102cfe041305ede3b6f743be967cea1
Hayk <hayk.sahakyan@codics.am>
2023-05-19 12:26:21 +0400

    json

--- js/script.js
+++ js/script.js
@@ -244,9 +244,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
 			form.insertAdjacentElement("afterend", statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open("POST", "server.php");
-			request.setRequestHeader("Content-type", "application/json");
 			const formData = new FormData(form);
 
 			const object = {};
@@ -254,19 +251,26 @@ window.addEventListener("DOMContentLoaded", () => {
 				object[key] = value;
 			});
 
-			const json = JSON.stringify(object);
-			request.send(json);
-
-			request.addEventListener("load", () => {
-				if (request.status === 200) {
-					console.log(request.response);
+			fetch("server.php", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify(object),
+			})
+				.then((data) => data.text())
+				.then((data) => {
+					console.log(data);
 					showThanksModal(message.success);
-					form.reset();
+
 					statusMessage.remove();
-				} else {
+				})
+				.catch(() => {
 					showThanksModal(message.failure);
-				}
-			});
+				})
+				.finally(() => {
+					form.reset();
+				});
 		});
 	}
 
@@ -292,4 +296,8 @@ window.addEventListener("DOMContentLoaded", () => {
 			closeModal();
 		}, 4000);
 	}
+
+	fetch("http://localhost:3000/menu")
+		.then((data) => data.json())
+		.then((res) => console.log(res));
 });
