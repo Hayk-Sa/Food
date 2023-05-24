60b6d54b9f58bfd62ea815bd02a91e0931be1945
Hayk <hayk.sahakyan@codics.am>
2023-05-24 12:42:46 +0400

    webpack

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-window.addEventListener("DOMContentLoaded", function () {
-	const tabs = require("./modules/tabs"),
-		modal = require("./modules/modal"),
-		timer = require("./modules/timer"),
-		cards = require("./modules/cards"),
-		calc = require("./modules/calc"),
-		forms = require("./modules/forms"),
-		slider = require("./modules/slider");
+require("es6-promise".polyfill());
+import "nodelist-foreach-polyfill";
 
-	tabs();
-	modal();
-	timer();
-	cards(); 
+import tabs from "./modules/tabs";
+import modal, { openModal } from "./modules/modal";
+import timer from "./modules/timer";
+import cards from "./modules/cards";
+import calc from "./modules/calc";
+import forms from "./modules/forms";
+import slider from "./modules/slider";
+
+window.addEventListener("DOMContentLoaded", () => {
+	const modalTimerId = setTimeout(
+		() => openModal(".modal", modalTimerId),
+		300000
+	);
+	tabs(".tabheader__item", ".tabcontent", ".tabheader__items");
+	modal("[data-modal]", ".modal", modalTimerId);
+	timer(".timer", "2024-06-11");
+	cards();
 	calc();
-	forms();
-	slider();
+	forms("form", modalTimerId);
+	slider({
+		container: ".offer__slider",
+		nextArrow: ".offer__slider-next",
+		slide: ".offer__slide",
+		prevArrow: ".offer__slider-prev",
+		totalCounter: "#total",
+		currentCounter: "#current",
+		wrapper: ".offer__slider-wrapper",
+		field: ".offer__slider-inner",
+	});
 });
